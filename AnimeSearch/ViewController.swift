6f406cc3fb3b8298fac0f414811faa22f6b164e6
Matheus <dio.nisio@live.com>
2023-12-19 12:05:08 -0300

    Initial Commit

---
+++ AnimeSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimeSearch
+//
+//  Created by Matheus Dionísio on 19/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
